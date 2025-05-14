3c60019f63a912ce1432cf49cfe777d8b71ad97f
Aliasgar-mullamitha <your.email@example.com>
2025-05-14 12:38:56 +0530

    commit

---
+++ 1.js
@@ -0,0 +1,6 @@
+console.log('hellow world');
+
+
+
+var n = 1;
+console.log(n);
---
+++ pr1.py
@@ -0,0 +1,17 @@
+# cricketer = ["abd","dvf","sxd"]
+# print(cricketer)
+# list1 = [1,2,1]
+# list2 = [1,2,3]
+
+# copy_list2 = list2.copy()
+# copy_list2.reverse()
+
+# if(copy_list2 == list1):
+#      print("palindrome")
+
+# else:
+#      print("not palindrome")
+
+grade = ["c","b","c","a","a","d"]
+grade.sort()
+print(grade)
